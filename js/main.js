bb0ce666a8d72f2758c5f248ac1dadf369f54462
valentin marta <valenmarta17@gmail.com>
2021-06-28 12:35:57 -0300

    desafio complementario clase 1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// desafio complementario
+alert('Hoy vamos a hacer sumas y multiplicaciones');
+
+let numero1 = Number(prompt('Por favor, ingrese un numero: '));
+
+let numero2 = Number(prompt('Ingrese un segundo numero: '));
+
+let suma = numero1 + numero2;
+
+let multiplicacion = numero1 * numero2;
+
+alert('Si sumamos esos numeros nos da: ' + suma);
+
+alert('Y si los multiplicamos nos da: ' + multiplicacion);
+
+alert('Saludos');
