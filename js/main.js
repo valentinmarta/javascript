3ed42b152ca19802bbd00af272b055b23dd05bde
valentin marta <valenmarta17@gmail.com>
2021-08-14 16:07:12 -0300

    desafio jquery

--- js/main.js
+++ js/main.js
@@ -1,79 +1,16 @@
-let nombre = prompt('Hola, ingresa tu nombre por favor: ');
+//interaccion con html para crear h1
 
-while (nombre == "") {
-    alert ('No ingresaste ningun nombre')
+$('#titulos').append('<h1>MI PAGINA</h1>');
 
-    nombre = prompt('Ingresa tu nombre porfavor')
-}
+//interaccion agregando h3 al titulo del formulario
 
-pregunta = prompt('De que pais sos?');
-
-alert('Hola ' + nombre + ' de ' + pregunta + ' bienvenido a mi pagina para sacar promedios');
-
-// Ciclo while para que menores de 18 no entren a la pagina
-alert('Por cuestiones de seguridad no podemos dejar que menores de 18 entren a la pagina');
-
-let repetir = parseInt(prompt('Cuantos años tenes?'));
-
-while (repetir < 18 ) {
-    alert ('Sos menor, no puedo dejar que ingreses a mi pagina');
-}
-
-alert ('Ahora si empecemos')
-
-// Pide numeros para mandarlos a la funcion y sacar promedios
-A = parseInt(prompt('Ingresa el primer numero:'));
-B = parseInt(prompt('Ingresa el segundo numero:'));
-
-function promedio() {
-    resultado = (A + B) / 2;
-    return resultado;
-}
-
-function mostrar(mensaje) {
-    alert('El promedio de esos dos numeros es: ' + mensaje);
-}
-
-promedio();
-mostrar(resultado);
-
-// Ciclo while por si lo quiere hacer de nuevo
-pregunta = prompt('Quieres hacerlo de nuevo?').toLowerCase();
-
-while (pregunta == 'si') {
-    A = parseInt(prompt('Ingresa el primer numero:'));
-    B = parseInt(prompt('Ingresa el segundo numero:'));
-
-    promedio();
-    mostrar(resultado);
-
-    pregunta = prompt('Quieres hacerlo de nuevo?').toLowerCase();
-}
-
-//interaccion con html que no me deja crear h1
-let titulo = document.createElement("div");
-
-titulo.innerHTML = "<h1>MI PAGINA</h1>"
-
-document.body.appendChild(titulo)
-
-//interaccion agregando h2 al titulo del formulario
-let padre = document.getElementById(form);
-
-let h2 = document.createElement("h2");
-
-h2.innerHTML = "<h2>Por favor complete los siguientes datos: </h2>"
-
-padre.appendChild(h2);
+$('#form').append('<h3>Por favor complete los siguientes datos: </h3>');
 
 //guardando los nombres de los creadores
 class persona{
     constructor (nombre, apellido) {
        this.nombre = nombre;
        this.apellido = apellido;
-    }
-    mostrar() {
-        alert (this.nombre + this.apellido);
     }
 }
 
