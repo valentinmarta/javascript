0ffd373a91b3272ab8a542fac0c8effe83431069
valentin marta <valenmarta17@gmail.com>
2021-08-28 16:36:36 -0300

    3era entrega proyecto final

--- js/main.js
+++ js/main.js
@@ -1,25 +1,14 @@
-//interaccion con html para crear h1
+// cuando ponemos el mouse sobre la info aparece
+$(".informacion").mouseover(() => {
+    $(".aparece").fadeIn(1500)
+})
 
-$('#titulos').append('<h1>MI PAGINA</h1>');
+//cuando ponemos el mouse sobre el footer aparece la info de contacto
+$("footer").mouseover(() => {
+    $(".footer").fadeIn(1500)
+})
 
-//interaccion agregando h3 al titulo del formulario
-
-$('#form').append('<h3>Por favor complete los siguientes datos: </h3>');
-
-//guardando los nombres de los creadores
-class persona{
-    constructor (nombre, apellido) {
-       this.nombre = nombre;
-       this.apellido = apellido;
-    }
-}
-
-let creador1 = new persona ('Valentin', ' Marta');
-
-let creador2 = new persona ('Valentin', ' Nicolas Marta');
-
-let creador3 = new persona ('Valentin', ' Marta Boggio')
-
-const creadores = [creador1, creador2, creador3]
-
-localStorage.setItem('creadores', JSON.stringify(creadores))
+//el titulo aparece lentamente
+setTimeout(() => {
+    $(".titulo1").fadeIn(1500)
+}, 400)
